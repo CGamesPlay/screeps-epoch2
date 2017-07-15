01e564245d998044dd47e5d9da8cf89807b90a4e
Ryan Patterson <cgamesplay@cgamesplay.com>
2017-07-15 15:11:41 -0700

    Now it uses generators

--- src/init.js
+++ src/init.js
@@ -1,3 +1,25 @@
-exports.loop = () => {
-  console.log(Game.time % 2 ? "Tick" : "Tock");
+function* main() {
+  let counter = 0;
+  while (true) {
+    console.log("Code has been running for", counter, "ticks");
+    yield null;
+    counter += 1;
+  }
+}
+
+exports.loop = function () {
+  var thread;
+  if (Memory.thread) {
+    try {
+      thread = regeneratorRuntime.deserializeGenerator(Memory.thread);
+    } finally {
+      delete Memory.thread;
+    }
+  } else {
+    thread = main();
+  }
+  let result = thread.next();
+  if (!result.done) {
+    Memory.thread = regeneratorRuntime.serializeGenerator(thread);
+  }
 };
